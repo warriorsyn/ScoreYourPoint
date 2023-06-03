6f50eea9167fb171af025d7b35aa4d597bd58dbc
Tatiwel <49209072+Tatiwel@users.noreply.github.com>
2023-06-03 20:59:44 -0300

    add entity framework

---
+++ ScoreYourPoint.Backend/ScoreYourPoint.Infra/Data/DataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ScoreYourPointApi.Infra.Data
+{
+
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("ScoreYourPointApiDatabase"), b => b.MigrationsAssembly("ScoreYourPointApi.Api"));
+        }
+
+    }
+}
